45d142699fb70fbc42aefc96cc6151c1f658a7e3
Pablo Hennique <phennique@gmail.com>
2023-09-08 11:03:01 -0400

    implemented post API and tested it

--- methods/post-request.js
+++ methods/post-request.js
@@ -1,1 +1,28 @@
-module.exports = (req, res) => {};
+const crypto = require("crypto");
+const requestBodyparser = require("../util/body-parser");
+const writeToFile = require("../util/write-to-file");
+
+module.exports = async (req, res) => {
+  if (req.url === "/api/movies") {
+    try {
+      let body = await requestBodyparser(req);
+      body.id = crypto.randomUUID();
+      req.movies.push(body);
+      writeToFile(req.movies);
+      res.writeHead(201, { "Content-Type": "application/json" });
+      res.end();
+    } catch (err) {
+      console.log(err);
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          title: "Validation failed",
+          message: "Request body is not valid",
+        })
+      );
+    }
+  } else {
+    res.writeHead(404, { "Content-type": "application/json" });
+    res.end(JSON.stringify({ title: "Not found", message: "Route not found" }));
+  }
+};
